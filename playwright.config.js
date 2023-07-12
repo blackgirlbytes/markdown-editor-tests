55da12c46b2fd3947750d9e44e06a950ad3e6d53
Rizel Scarlett <blackgirlbytes@github.com>
2023-07-12 02:47:00 -0400

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -75,7 +75,6 @@ module.exports = defineConfig({
   webServer: {
     command: 'npm run dev',
     url: 'http://127.0.0.1:3001',
-    timeout: 150000,
     reuseExistingServer: !process.env.CI,
   },
 });
